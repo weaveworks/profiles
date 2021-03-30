4364cdad5ebdfd7848baadf7186517426227aff3
cpu1 <patwal.chetan@gmail.com>
2021-03-30 19:38:08 +0530

    Add integration tests for supplying values to HelmRelease

--- controllers/profile_controller_test.go
+++ controllers/profile_controller_test.go
@@ -9,14 +9,18 @@ import (
 	sourcev1 "github.com/fluxcd/source-controller/api/v1beta1"
 	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	"github.com/weaveworks/profiles/api/v1alpha1"
 	v1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var _ = Describe("ProfileController", func() {
+	const nginxProfileURL = "https://github.com/weaveworks/nginx-profile"
+
 	var (
 		namespace string
 		ctx       = context.Background()
@@ -33,64 +37,85 @@ var _ = Describe("ProfileController", func() {
 	})
 
 	Context("Create", func() {
-		When("the Profile contains a single local Helm Chart", func() {
-			It("creates the correct resources", func() {
-				subscriptionName := "foo"
-				branch := "main"
-				profileURL := "https://github.com/weaveworks/nginx-profile"
+		DescribeTable("Applying a Profile creates the correct resources", func(pSubSpec v1alpha1.ProfileSubscriptionSpec) {
+			subscriptionName := "foo"
+			branch := "main"
 
-				pSub := v1alpha1.ProfileSubscription{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "ProfileSubscription",
-						APIVersion: "profilesubscriptions.weave.works/v1alpha1",
+			pSub := v1alpha1.ProfileSubscription{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ProfileSubscription",
+					APIVersion: "profilesubscriptions.weave.works/v1alpha1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      subscriptionName,
+					Namespace: namespace,
+				},
+			}
+			pSub.Spec = pSubSpec
+			Expect(k8sClient.Create(ctx, &pSub)).Should(Succeed())
+
+			By("creating a GitRepository resource")
+			profileRepoName := "nginx-profile"
+			gitRepoName := fmt.Sprintf("%s-%s-%s", subscriptionName, profileRepoName, branch)
+			gitRepo := sourcev1.GitRepository{}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, client.ObjectKey{Name: gitRepoName, Namespace: namespace}, &gitRepo)
+			}, 10*time.Second).ShouldNot(HaveOccurred())
+			Expect(gitRepo.Spec.URL).To(Equal(nginxProfileURL))
+			Expect(gitRepo.Spec.Reference.Branch).To(Equal(branch))
+
+			By("creating a HelmRelease resource")
+			profileName := "nginx"
+			chartName := "nginx-server"
+			helmReleaseName := fmt.Sprintf("%s-%s-%s", subscriptionName, profileName, chartName)
+			helmRelease := helmv2.HelmRelease{}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, client.ObjectKey{Name: helmReleaseName, Namespace: namespace}, &helmRelease)
+			}, 10*time.Second).ShouldNot(HaveOccurred())
+			Expect(helmRelease.Spec.Chart.Spec.Chart).To(Equal("nginx/chart"))
+			Expect(helmRelease.Spec.Chart.Spec.SourceRef).To(Equal(
+				helmv2.CrossNamespaceObjectReference{
+					Kind:      "GitRepository",
+					Name:      gitRepoName,
+					Namespace: namespace,
+				},
+			))
+			if pSub.Spec.Values != nil {
+				Expect(helmRelease.Spec.Values).To(Equal(pSub.Spec.Values))
+			}
+			if pSub.Spec.ValuesFrom != nil {
+				Expect(helmRelease.Spec.ValuesFrom).To(Equal(pSub.Spec.ValuesFrom))
+			}
+
+			By("updating the status")
+			profile := v1alpha1.ProfileSubscription{}
+			Eventually(func() string {
+				Expect(k8sClient.Get(ctx, client.ObjectKey{Name: subscriptionName, Namespace: namespace}, &profile)).To(Succeed())
+				return profile.Status.State
+			}, 10*time.Second).Should(Equal("running"))
+		},
+			Entry("a single Helm chart with no supplied values", v1alpha1.ProfileSubscriptionSpec{
+				ProfileURL: nginxProfileURL,
+			}),
+			Entry("a single Helm chart with supplied values", v1alpha1.ProfileSubscriptionSpec{
+				ProfileURL: nginxProfileURL,
+				Values: &apiextensionsv1.JSON{
+					Raw: []byte(`{"replicaCount": 3,"service":{"port":8081}}`),
+				},
+			}),
+			Entry("a single Helm chart with values supplied via valuesFrom", v1alpha1.ProfileSubscriptionSpec{
+				ProfileURL: nginxProfileURL,
+				ValuesFrom: []helmv2.ValuesReference{
+					{
+						Name:     "nginx-values",
+						Kind:     "Secret",
+						Optional: true,
 					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      subscriptionName,
-						Namespace: namespace,
-					},
-					Spec: v1alpha1.ProfileSubscriptionSpec{
-						ProfileURL: profileURL,
-					},
-				}
-				Expect(k8sClient.Create(ctx, &pSub)).Should(Succeed())
+				},
+			}),
+		)
 
-				By("creating a GitRepository resource")
-				profileRepoName := "nginx-profile"
-				gitRepoName := fmt.Sprintf("%s-%s-%s", subscriptionName, profileRepoName, branch)
-				gitRepo := sourcev1.GitRepository{}
-				Eventually(func() error {
-					return k8sClient.Get(ctx, client.ObjectKey{Name: gitRepoName, Namespace: namespace}, &gitRepo)
-				}, 10*time.Second).ShouldNot(HaveOccurred())
-				Expect(gitRepo.Spec.URL).To(Equal(profileURL))
-				Expect(gitRepo.Spec.Reference.Branch).To(Equal(branch))
-
-				By("creating a HelmRelease resource")
-				profileName := "nginx"
-				chartName := "nginx-server"
-				helmReleaseName := fmt.Sprintf("%s-%s-%s", subscriptionName, profileName, chartName)
-				helmRelease := helmv2.HelmRelease{}
-				Eventually(func() error {
-					return k8sClient.Get(ctx, client.ObjectKey{Name: helmReleaseName, Namespace: namespace}, &helmRelease)
-				}, 10*time.Second).ShouldNot(HaveOccurred())
-				Expect(helmRelease.Spec.Chart.Spec.Chart).To(Equal("nginx/chart"))
-				Expect(helmRelease.Spec.Chart.Spec.SourceRef).To(Equal(
-					helmv2.CrossNamespaceObjectReference{
-						Kind:      "GitRepository",
-						Name:      gitRepoName,
-						Namespace: namespace,
-					},
-				))
-
-				By("updating the status")
-				profile := v1alpha1.ProfileSubscription{}
-				Eventually(func() string {
-					Expect(k8sClient.Get(ctx, client.ObjectKey{Name: subscriptionName, Namespace: namespace}, &profile)).To(Succeed())
-					return profile.Status.State
-				}, 10*time.Second).Should(Equal("running"))
-			})
-		})
-
-		When("the retrieving the Profile Definition fails", func() {
+		When("retrieving the Profile Definition fails", func() {
 			It("updates the status", func() {
 				subscriptionName := "fetch-definition-error"
 				profileURL := "https://github.com/does-not/exist"
@@ -121,10 +146,10 @@ var _ = Describe("ProfileController", func() {
 			})
 		})
 
-		When("the creating Profile artifacts fail", func() {
+		When("creating Profile artifacts fail", func() {
 			It("updates the status", func() {
 				subscriptionName := "git-resource-already-exists-error"
-				profileURL := "https://github.com/weaveworks/nginx-profile"
+				profileURL := nginxProfileURL
 
 				gitRefName := fmt.Sprintf("%s-%s-%s", subscriptionName, "nginx-profile", "main")
 				gitRepo := sourcev1.GitRepository{
