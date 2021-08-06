0ec96b40dbaa15b5e4f072e73eb33bf8d494a175
gh-actions <actions@github.com>
2021-08-06 15:51:03 +0000

    Deploy website - based on b59ad5790bedf2a42256a2d55b5f120c5614a834

---
+++ assets/js/7eb5bf2d.9b86d117.js
@@ -0,0 +1,1 @@
+(self.webpackChunkprofiles_dev=self.webpackChunkprofiles_dev||[]).push([[4],{6667:function(e){"use strict";e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"profile","name":"profile","count":1,"permalink":"/blog/tags/profile"}')}}]);
---
+++ assets/js/814f3328.cbff65f7.js
@@ -0,0 +1,1 @@
+(self.webpackChunkprofiles_dev=self.webpackChunkprofiles_dev||[]).push([[2535],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Install MLOps Profile with Weave GitOps","permalink":"/blog/mlops-profile"}]}')}}]);
